e886891c7186008be99f732e4e506f781722079c
RomanenkoAndrew <romanenkoandrey95@mail.ru>
2021-02-09 16:03:09 +0300

    feat: complete authorization

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,6 +3,8 @@ import is from "is_js";
 import styles from "./Auth.module.css";
 import Button from "../../components/UI/Button/Button";
 import Input from "../../components/UI/Input/Input";
+import axios from "axios";
+import { API_KEY } from "../../Constants/Constants";
 
 class Auth extends Component {
   state = {
@@ -28,8 +30,38 @@ class Auth extends Component {
       },
     },
   };
-  loginHandler = () => {};
-  registerHandler = () => {};
+  loginHandler = async () => {
+    const authData = {
+      email: this.state.formControls.email.value,
+      password: this.state.formControls.password.value,
+      returnSecureToken: true,
+    };
+    try {
+      const response = await axios.post(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_KEY}`,
+        authData
+      );
+      console.log(response);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  registerHandler = async () => {
+    const authData = {
+      email: this.state.formControls.email.value,
+      password: this.state.formControls.password.value,
+      returnSecureToken: true,
+    };
+    try {
+      const response = await axios.post(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
+        authData
+      );
+      console.log(response);
+    } catch (e) {
+      console.log(e);
+    }
+  };
   submitHandler = (event) => {
     event.preventDefault();
   };
